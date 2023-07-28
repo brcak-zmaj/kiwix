c71f0c8a23ed55fe42a16ef322e7de175e27e335
Almir <64784654+brcak-zmaj@users.noreply.github.com>
2023-07-28 11:46:09 -0400

    Create zim_library_gen.sh

---
+++ zim_library_gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define directory that contains your ZIM files
+ZIM_DIR="/mnt/archives/zim"
+
+# Path to the library file
+LIBRARY_FILE="${ZIM_DIR}/library.xml"
+
+# Create or clear the library file
+echo "" > "${LIBRARY_FILE}"
+
+# Find all ZIM files in the directory and add them to the library
+for ZIM_FILE in ${ZIM_DIR}/*.zim; do
+    kiwix-manage "${LIBRARY_FILE}" add "${ZIM_FILE}"
+done
