baa5410a8ce24f9701dff2b87f1bb34139d1a153
Almir <64784654+brcak-zmaj@users.noreply.github.com>
2023-07-28 11:57:39 -0400

    Update zim_library_gen.sh

--- zim_library_gen.sh
+++ zim_library_gen.sh
@@ -9,6 +9,8 @@ LIBRARY_FILE="${ZIM_DIR}/library.xml"
 # Create or clear the library file
 echo "" > "${LIBRARY_FILE}"
 
+sudo apt install kiwix-tools -y
+
 # Find all ZIM files in the directory and add them to the library
 for ZIM_FILE in ${ZIM_DIR}/*.zim; do
     kiwix-manage "${LIBRARY_FILE}" add "${ZIM_FILE}"
